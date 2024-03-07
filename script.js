3305dca0089189fc17fa0fa47a9c0dd152f3e17d
DaleLore <DaleLoreNY@gmail.com>
2024-03-07 12:10:20 -0500

    Chrome dino game

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-var dino = document.getElementById("dino");
+const dino = document.getElementById("dino");
 var cactus = document.getElementById("cactus");
 
 function jump(){
-    if(dino.classList != "animate"){
-        dino.classList.add("animate");
+    if(dino.classList != "jump"){
+        dino.classList.add("jump");
     }
     
     setTimeout(function(){
-        dino.classList.remove("animate");
-    },500);
+        dino.classList.remove("jump")
+    }, 300)
 }
+
+let collision = setInterval(function(){
+
+    // Get Dino Y position
+   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+   // console.log("dino", dinoTop); 
+
+   // Get Cactus X position
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    // console.log("cactus", cactusLeft);
+
+
+    // Crash
+    if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+        // console.log("collision");
+        cactus.style.animation = "none";
+        cactus.style.display = "none";
+        alert("Whump whump");
+
+    }
+
+}, 10)
+
+
+document.addEventListener("keyup", function(event){
+    jump();
+})
+
