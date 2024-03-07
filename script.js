7482e9ea882e471014df3cdd161896ef7750e1d0
DaleLore <DaleLoreNY@gmail.com>
2024-03-06 23:19:34 -0500

    Dino jumps on every click

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ var dino = document.getElementById("dino");
 var cactus = document.getElementById("cactus");
 
 function jump(){
-    dino.classList.add("animate");
+    if(dino.classList != "animate"){
+        dino.classList.add("animate");
+    }
+    
+    setTimeout(function(){
+        dino.classList.remove("animate");
+    },500);
 }
